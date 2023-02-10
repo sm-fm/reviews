b0cdd17db23cc0c116414fac4bd869b6244e80b6
Sean McDaniel <sm.fm.static@gmail.com>
2023-02-10 00:29:31 -0500

    Refactored db connection file to use env file for database credentials

--- db/index.js
+++ db/index.js
@@ -1,11 +1,11 @@
 require('dotenv').config();
 const Pool = require('pg-pool');
 const pool = new Pool ({
-  host: 'ec2-18-225-35-250.us-east-2.compute.amazonaws.com',
+  host: process.env.HOST,
   port: 5432,
   database: 'reviews',
-  user: 'seanmcdaniel',
-  password: 'password',
+  user: process.env.USER,
+  password: process.env.PASSWORD,
   max: 20,
   idleTimeoutMillis: 1000
 });
